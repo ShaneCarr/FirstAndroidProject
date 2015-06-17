a334dd7a6d73be01f844e61f7e58c2ad6c3afff4
ShaneCarr <shanepcarr@hotmail.com>
2015-06-16 21:16:54 -0700

    test

--- app/src/main/java/com/microsoft/myfirstapp/MyActivity.java
+++ app/src/main/java/com/microsoft/myfirstapp/MyActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-// some conflicting change
+// 2 some conflicting change
 public class MyActivity extends AppCompatActivity {
 
     @Override
